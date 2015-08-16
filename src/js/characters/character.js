cb645c47dca6de81d95a4c2478a796e04fb97f51
Grzegorz Tanczyk <fb@tanczyk.pl>
2015-08-16 16:53:31 +0200

    commit missing character directory

---
+++ src/js/characters/character.js
@@ -0,0 +1,32 @@
+/**
+ * @param {GameWorld} world
+ * @param {Race} race
+ * @param {BoatObject} boat
+ * @constructor
+ */
+function Character(world, race, boat) {
+    this.world = world;
+    this.race = race;
+    this.boat = boat;
+};
+
+Character.prototype.update = function() {
+    var waypoint = this.race.getNextWaypoint(this.boat);
+    
+    if (!waypoint) {
+        this.boat.straight();
+        return;
+    }
+    
+    var direction = VMath.angle(
+                        VMath.normalize(VMath.sub(waypoint.vec(), this.boat.vec())),
+                        [Math.cos(this.boat.getDirection()), Math.sin(this.boat.getDirection())]
+                    );
+    if (Math.abs(direction) < VMath.EPSILON) {
+        this.boat.straight();
+    } else if (direction < 0) {
+        this.boat.turnRight();
+    } else {
+        this.boat.turnLeft();
+    }
+};
