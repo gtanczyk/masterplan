a51dd8e565de423049654866428d46e0401b99f1
Grzegorz Tanczyk <fb@tanczyk.pl>
2015-08-17 23:03:27 +0200

    add annoying character

--- src/js/states/state-game-init.js
+++ src/js/states/state-game-init.js
@@ -41,7 +41,7 @@ function stateGameInit() {
     race.addCharacter(new SprinterCharacter(world, race, boat2));
     race.addCharacter(new BonuserCharacter(world, race, boat3));
     race.addCharacter(new Character(world, race, boat4));
-    race.addCharacter(new Character(world, race, boat5));
+    race.addCharacter(new AnnoyingCharacter(world, race, boat5));
     race.addCharacter(new Character(world, race, boat6));
     
     var HUD = new GameHUD(race, world, boat);
